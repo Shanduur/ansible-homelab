a14e83422a3c3f6ab846712f06a8011548c07d46
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2020-12-27 19:50:24 +0100

    feat: changed tasks to own

---
+++ templates/shell/.common.sh
@@ -0,0 +1,12 @@
+alias cls="cd && clear"
+alias dir="dir --color=auto"
+alias grep="grep --color=auto"
+alias ls="ls --color=auto"
+alias ll="ls -la --color=auto"
+alias la="ls -a --color=auto"
+alias l="ls -l --color=auto"
+alias python="python3"
+
+export LANG="en_US.UTF-8"
+export PATH=$PATH:/.local/bin
+export TZ="Europe/Warsaw"
